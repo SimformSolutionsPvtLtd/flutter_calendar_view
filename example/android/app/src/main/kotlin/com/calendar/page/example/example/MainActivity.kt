70397ea0eeb885aa3d4e377bd96f6ac6be60db90
Parth Baraiya <parth.b@simformsolutions.com>
2021-05-26 17:04:06 +0530

    :feature added project files

---
+++ example/android/app/src/main/kotlin/com/calendar/page/example/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.calendar.page.example.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/flutter_calendar_page.dart
@@ -0,0 +1,7 @@
+library flutter_calendar_page;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
