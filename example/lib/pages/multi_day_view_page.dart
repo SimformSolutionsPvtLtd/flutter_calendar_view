af03798bf774bc9ab70be1ecf5537eaaf68f085e
lavigarg-simform <lavi.garg@simformsolutions.com>
2026-05-11 17:12:50 +0530

    fix: 🔨 extend scaffold background behind status bar in multi day view example

--- example/lib/pages/multi_day_view_page.dart
+++ example/lib/pages/multi_day_view_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../enumerations.dart';
 import '../extension.dart';
-import '../widgets/responsive_widget.dart';
 import '../widgets/multi_day_view_widget.dart';
+import '../widgets/responsive_widget.dart';
 import 'create_event_page.dart';
 import 'web/web_home_page.dart';
 
@@ -20,6 +20,8 @@ class _MultiDayViewDemoState extends State<MultiDayViewDemo> {
     return ResponsiveWidget(
       webWidget: WebHomePage(selectedView: CalendarView.week),
       mobileWidget: Scaffold(
+        primary: false,
+        appBar: AppBar(leading: const SizedBox.shrink()),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add, color: context.appColors.onPrimary),
           elevation: 8,
